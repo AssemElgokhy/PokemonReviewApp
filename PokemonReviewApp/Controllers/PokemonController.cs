91ef45e0f161179dff2d5777c4d8d2c945cdfbdc
assem elgokhy <319190407@sha.edu.eg>
2023-02-18 19:14:13 +0200

    Add project files.

---
+++ PokemonReviewApp/Controllers/PokemonController.cs
@@ -0,0 +1,64 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Dto;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokemonController : Controller
+    {
+        private readonly IPokemonRepository _pokemonRepository;
+        private readonly IMapper _mapper;
+
+        public PokemonController(IPokemonRepository pokemonRepository, IMapper mapper)
+        {
+            _pokemonRepository = pokemonRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Pokemon>))]
+        public IActionResult GetPokemons()
+        {
+            var pokemons =_mapper.Map<List<PokemonDto>>(_pokemonRepository.GetPokemons());
+
+            
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(pokemons);
+        }
+
+        [HttpGet("{PokeId}")]
+        [ProducesResponseType(200, Type=typeof(Pokemon))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPokemon(int PokeId) 
+        {
+            if (!_pokemonRepository.PokemonExists(PokeId))
+                return NotFound();
+            var pokemon = _mapper.Map<PokemonDto>(_pokemonRepository.GetPokemon(PokeId));
+
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+            return Ok(pokemon);
+        }
+        [HttpGet("{PokeId}/rating")]
+        [ProducesResponseType(200, Type = typeof(Pokemon))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPokemonRating(int PokeId)
+        {
+            if (!_pokemonRepository.PokemonExists(PokeId))
+                return NotFound();
+            var rating  = _pokemonRepository.GetPokemonRating(PokeId);
+
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+            return Ok(rating);
+        }
+
+
+    }
+}
---
+++ PokemonReviewApp/Interfaces/ICountryRepository.cs
@@ -0,0 +1,13 @@
+﻿using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Interfaces
+{
+    public interface ICountryRepository
+    {
+        ICollection<Country> GetCountries();
+        Country GetCountry(int id);
+        Country GetCountryByOwner(int OwnerId);
+        ICollection<Owner> GetOwnersFromACountry(int CountryId);
+        bool CountryExists(int id);
+    }
+}
